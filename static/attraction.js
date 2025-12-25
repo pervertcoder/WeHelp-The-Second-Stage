62d1f10c326c8e08e8643a44b7c4dbfc7680ba34
Pervertcoder <lance60702@gmail.com>
2025-12-25 12:35:01 +0800

    12/25 css layout problems fixed button left

--- static/attraction.js
+++ static/attraction.js
@@ -18,10 +18,10 @@ const radioAll = document.getElementsByName("time");
 const price = document.querySelector(".priceS");
 const priceChanged = function () {
   if (radioAll[0].checked) {
-    price.textContent = "新台幣2000元";
+    price.textContent = "新台幣 2000元";
   }
   if (radioAll[1].checked) {
-    price.textContent = "新台幣2500元";
+    price.textContent = "新台幣 2500元";
   }
 };
 for (let i = 0; i < radioAll.length; i++) {
